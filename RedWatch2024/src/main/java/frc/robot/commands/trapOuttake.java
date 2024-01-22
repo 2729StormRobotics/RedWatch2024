97040402773c17ac65cf169fd979f37682c4036e
AmanullahKM <148914568+AmanullahKM@users.noreply.github.com>
2024-01-22 17:11:44 -0500

    Update and rename outake.java to trapOuttake.java

---
+++ RedWatch2024/src/main/java/frc/robot/commands/trapOuttake.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.motor;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class trapOuttake extends CommandBase {
+    /** Initialize */
+    private final TrapSubsystem m_Trap;
+
+    public trapOuttake(TrapSubsystem trap) {
+        // Use addRequirements() here to declare subsystem dependencies.
+        m_Trap = trap;
+        addRequirements(m_Trap);
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {}
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        m_Trap.handOuttake();
+
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        m_Trap.stopHandMotor();
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        
+    }
+}
