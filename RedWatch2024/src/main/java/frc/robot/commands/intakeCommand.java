3f51183e649bec47e9ee4146609f1c4e1d73e324
AmanullahKM <148914568+AmanullahKM@users.noreply.github.com>
2024-01-22 16:45:48 -0500

    Update intakeCommand.java

--- RedWatch2024/src/main/java/frc/robot/commands/intakeCommand.java
+++ RedWatch2024/src/main/java/frc/robot/commands/intakeCommand.java
@@ -25,7 +25,7 @@ public class intakeCommand extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        
+        m_Trap.handIntake();
 
     }
 
