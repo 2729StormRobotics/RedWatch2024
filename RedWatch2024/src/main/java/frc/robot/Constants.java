3901e6d23da4856ea8467ecfc06b32edc944bbfa
AmanullahKM <148914568+AmanullahKM@users.noreply.github.com>
2024-01-24 23:50:31 -0500

    Update Constants.java

--- RedWatch2024/src/main/java/frc/robot/Constants.java
+++ RedWatch2024/src/main/java/frc/robot/Constants.java
@@ -16,4 +16,13 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+
+  public static class TrapConstants {
+    // Need to add proper port numbers
+    public static final int kHandMotorID = 0;
+    public static final int kArmMotorID = 0;
+    public static final double kHandMotorSpeed = 0.4;
+    public static final double kArmMotorSpeed = 0.1;
+  }
+  
 }
