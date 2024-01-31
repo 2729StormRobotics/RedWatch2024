8834b241c604bd444686036b96dc5564f76b1c43
PraveenPrabaharan <83364745+PraveenPrabaharan@users.noreply.github.com>
2024-01-31 17:05:07 -0500

    Fixed robot container
    Fixed the work on robot container.

--- RedWatch2024/src/main/java/frc/robot/subsystems/Indexer.java
+++ RedWatch2024/src/main/java/frc/robot/subsystems/Indexer.java
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
-import frc.robot.Constants;
 import frc.robot.Constants.IndexerConstants;
 
 public class Indexer extends SubsystemBase {
