110d72f621b956423e3ef31c5247402b41271320
PraveenPrabaharan <83364745+PraveenPrabaharan@users.noreply.github.com>
2024-02-05 15:25:10 -0500

    small tweaks

--- RedWatch2024/src/main/java/frc/robot/commandgroups/AutoPivot.java
+++ RedWatch2024/src/main/java/frc/robot/commandgroups/AutoPivot.java
@@ -2,9 +2,10 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.Shooter;
+package frc.robot.commandgroups;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.Shooter.Pivot;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Vision;
 
@@ -12,12 +13,16 @@ import frc.robot.subsystems.Vision;
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class AutoPivot extends SequentialCommandGroup {
+  private final Shooter m_shooter;
+  private final Vision m_vision;
   /** Creates a new AutoPivot. */
   public AutoPivot(Shooter shooter, Vision vision) {
+    m_shooter = shooter;
+    m_vision = vision;
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-      new Pivot(shooter, shooter.getOptimalAngle(0, vision.getSpeakerDistance())) // Pivot to the angle returned by the getOptimalAngle() method
+      new Pivot(m_shooter, m_shooter.getOptimalAngle(0, m_vision.getSpeakerDistance())) // Pivot to the angle returned by the getOptimalAngle() method
     );
   }
 }
