859ad903e38dae18ca2f9f2892707cb455bbfbcc
AmanullahKM <135625@lrstudents.org>
2024-02-09 15:54:59 -0500

    Added Rev up animation

---
+++ RedWatch2024/src/main/java/frc/robot/presets/AnimationRevUp.java
@@ -0,0 +1,153 @@
+package frc.robot.presets;
+
+import frc.robot.subsystems.LEDs.Color;
+
+public class AnimationRevUp {
+    // colors:
+    // Team colors
+    public static final Color red = new Color(255, 0, 0);
+    // public static final Color black = new Color(255, 230, 220);
+    public static final Color black = new Color(0, 0, 0);
+    public static final Color brown = new Color(139, 69, 19);
+
+    // Game piece colors
+    public static final Color yellow = new Color(242, 60, 0);
+    public static final Color purple = new Color(184, 0, 185);
+
+    // Indicator colors
+    public static final Color white = new Color(255, 230, 220);
+    public static final Color green = new Color(56, 209, 0);
+    public static final Color blue = new Color(8, 32, 255);
+    public static final Color orange = new Color(255, 25, 0);
+    public static final Color skin = new Color(169, 125, 100);
+
+    public static Color[] frame1_RevUp = {
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            red, black, black, black, black, black, black, black, black, black,
+    };
+
+    public static Color[] frame2_RevUp = {
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, red, red,
+            red, black, black, black, black, black, black, black, black, black,
+    };
+
+    public static Color[] frame3_RevUp = {
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            red, red, red, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, red, red,
+            red, black, black, black, black, black, black, black, black, black,
+    };
+
+    public static Color[] frame4_RevUp = {
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, orange, orange, orange, orange,
+            red, red, red, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, red, red,
+            red, black, black, black, black, black, black, black, black, black,
+    };
+
+    public static Color[] frame5_RevUp = {
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            orange, orange, orange, orange, orange, black, black, black, black, black,
+            black, black, black, black, black, black, orange, orange, orange, orange,
+            red, red, red, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, red, red,
+            red, black, black, black, black, black, black, black, black, black,
+    };
+
+    public static Color[] frame6_RevUp = {
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, orange, orange, orange, orange, orange, orange,
+            orange, orange, orange, orange, orange, black, black, black, black, black,
+            black, black, black, black, black, black, orange, orange, orange, orange,
+            red, red, red, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, red, red,
+            red, black, black, black, black, black, black, black, black, black,
+    };
+
+    public static Color[] frame7_RevUp = {
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            orange, orange, orange, orange, orange, orange, orange, black, black, black,
+            black, black, black, black, orange, orange, orange, orange, orange, orange,
+            orange, orange, orange, orange, orange, black, black, black, black, black,
+            black, black, black, black, black, black, orange, orange, orange, orange,
+            red, red, red, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, red, red,
+            red, black, black, black, black, black, black, black, black, black,
+    };
+
+    public static Color[] frame8_RevUp = {
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, yellow, yellow, yellow, yellow, yellow, yellow, yellow, yellow,
+            orange, orange, orange, orange, orange, orange, orange, black, black, black,
+            black, black, black, black, orange, orange, orange, orange, orange, orange,
+            orange, orange, orange, orange, orange, black, black, black, black, black,
+            black, black, black, black, black, black, orange, orange, orange, orange,
+            red, red, red, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, red, red,
+            red, black, black, black, black, black, black, black, black, black,
+    };
+
+    public static Color[] frame9_RevUp = {
+            black, black, black, black, black, black, black, black, black, black,
+            yellow, yellow, yellow, yellow, yellow, yellow, yellow, yellow, yellow, black,
+            black, black, yellow, yellow, yellow, yellow, yellow, yellow, yellow, yellow,
+            orange, orange, orange, orange, orange, orange, orange, black, black, black,
+            black, black, black, black, orange, orange, orange, orange, orange, orange,
+            orange, orange, orange, orange, orange, black, black, black, black, black,
+            black, black, black, black, black, black, orange, orange, orange, orange,
+            red, red, red, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, red, red,
+            red, black, black, black, black, black, black, black, black, black,
+    };
+
+    public static Color[] frame10_RevUp = {
+            yellow, yellow, yellow, yellow, yellow, yellow, yellow, yellow, yellow, yellow,
+            yellow, yellow, yellow, yellow, yellow, yellow, yellow, yellow, yellow, black,
+            black, black, yellow, yellow, yellow, yellow, yellow, yellow, yellow, yellow,
+            orange, orange, orange, orange, orange, orange, orange, black, black, black,
+            black, black, black, black, orange, orange, orange, orange, orange, orange,
+            orange, orange, orange, orange, orange, black, black, black, black, black,
+            black, black, black, black, black, black, orange, orange, orange, orange,
+            red, red, red, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, red, red,
+            red, black, black, black, black, black, black, black, black, black,
+    };
+}
