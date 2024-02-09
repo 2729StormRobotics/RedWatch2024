c2407bfcaf0d32b6038c73712d7d30cec2d84545
i-am-your-pet-fish <118485273+i-am-your-pet-fish@users.noreply.github.com>
2024-02-09 09:17:12 -0500

    changed a few names, (May have naming error for preset (2720Animation_Red))

--- RedWatch2024/src/main/java/frc/robot/commands/runMatrixAnimation.java
+++ RedWatch2024/src/main/java/frc/robot/commands/runMatrixAnimation.java
@@ -9,45 +9,46 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.LEDs;
 import frc.robot.subsystems.LEDs.Color;
 import frc.robot.presets.matrixPresets;
+import frc.robot.presets.2720Animation_Red;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class runMatrixAnimation extends SequentialCommandGroup {
   /** Creates a new runMatrixAnimation. */
-  Color[][] colors = {
-    matrixPresets.frame1_2720,
-    matrixPresets.frame2_2720,
-    matrixPresets.frame3_2720,
-    matrixPresets.frame4_2720,
-    matrixPresets.frame5_2720,
-    matrixPresets.frame6_2720,
-    matrixPresets.frame7_2720,
-    matrixPresets.frame8_2720,
-    matrixPresets.frame9_2720,
-    matrixPresets.frame10_2720,
-    matrixPresets.frame11_2720,
-    matrixPresets.frame12_2720,
-    matrixPresets.frame13_2720,
-    matrixPresets.frame14_2720,
-    matrixPresets.frame15_2720,
-    matrixPresets.frame16_2720,
-    matrixPresets.frame17_2720,
-    matrixPresets.frame18_2720,
-    matrixPresets.frame19_2720,
-    matrixPresets.frame20_2720,
-    matrixPresets.frame21_2720,
-    matrixPresets.frame22_2720,
-    matrixPresets.frame23_2720
+  Color[][] Red2720Animation = {
+    2720Animation_Red.frame1_2720,
+    2720Animation_Red.frame2_2720,
+    2720Animation_Red.frame3_2720,
+    2720Animation_Red.frame4_2720,
+    2720Animation_Red.frame5_2720,
+    2720Animation_Red.frame6_2720,
+    2720Animation_Red.frame7_2720,
+    2720Animation_Red.frame8_2720,
+    2720Animation_Red.frame9_2720,
+    2720Animation_Red.frame10_2720,
+    2720Animation_Red.frame11_2720,
+    2720Animation_Red.frame12_2720,
+    2720Animation_Red.frame13_2720,
+    2720Animation_Red.frame14_2720,
+    2720Animation_Red.frame15_2720,
+    2720Animation_Red.frame16_2720,
+    2720Animation_Red.frame17_2720,
+    2720Animation_Red.frame18_2720,
+    2720Animation_Red.frame19_2720,
+    2720Animation_Red.frame20_2720,
+    2720Animation_Red.frame21_2720,
+    2720Animation_Red.frame22_2720,
+    2720Animation_Red.frame23_2720
   };
   public runMatrixAnimation(LEDs m_leds) {
 
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    for (int i = 0; i < colors.length; ++i) {
+    for (int i = 0; i < Red2720Animation.length; ++i) {
       addCommands(
-        new setMatrix(colors[i], m_leds),
-        new WaitCommand(1)
+        new setMatrix(Red2720Animation[i], m_leds),
+        new WaitCommand(0.1)
       );
     }
   }
