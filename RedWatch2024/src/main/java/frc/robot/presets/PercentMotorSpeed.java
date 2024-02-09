8fe98eea60d83003ba348afc89db13c9ed580890
AmanullahKM <135625@lrstudents.org>
2024-02-09 16:43:21 -0500

    Added animation preset for motor speed

---
+++ RedWatch2024/src/main/java/frc/robot/presets/PercentMotorSpeed.java
@@ -0,0 +1,149 @@
+package frc.robot.presets;
+
+import frc.robot.subsystems.LEDs.Color;
+
+public class PercentMotorSpeed {
+    // Gradient colors:
+    public static final Color black = new Color(0, 0, 0);
+
+    public static final Color red1 = new Color(255, 0, 0);
+    public static final Color red2 = new Color(247, 65, 0);
+    public static final Color orange1 = new Color(247, 65, 0);
+    public static final Color orange2 = new Color(247, 65, 0);
+    public static final Color orange3 = new Color(247, 65, 0);
+    public static final Color yellow1 = new Color(247, 65, 0);
+    public static final Color green1 = new Color(247, 65, 0);
+    public static final Color green2 = new Color(247, 65, 0);
+    public static final Color green3 = new Color(247, 65, 0);
+    public static final Color green4 = new Color(247, 65, 0);
+
+    public static Color[] frame1_10per = {
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            red1, red1, red1, red1, red1, red1, red1, red1, red1, red1,
+    };
+
+    public static Color[] frame2_20per = {
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            red2, red2, red2, red2, red2, red2, red2, red2, red2, red2,
+            red1, red1, red1, red1, red1, red1, red1, red1, red1, red1,
+    };
+
+    public static Color[] frame3_30per = {
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            orange1, orange1, orange1, orange1, orange1, orange1, orange1, orange1, orange1, orange1,
+            red2, red2, red2, red2, red2, red2, red2, red2, red2, red2,
+            red1, red1, red1, red1, red1, red1, red1, red1, red1, red1,
+    };
+
+    public static Color[] frame4_40per = {
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            orange2, orange2, orange2, orange2, orange2, orange2, orange2, orange2, orange2, orange2,
+            orange1, orange1, orange1, orange1, orange1, orange1, orange1, orange1, orange1, orange1,
+            red2, red2, red2, red2, red2, red2, red2, red2, red2, red2,
+            red1, red1, red1, red1, red1, red1, red1, red1, red1, red1
+    };
+
+    public static Color[] frame5_50per = {
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            orange3, orange3, orange3, orange3, orange3, orange3, orange3, orange3, orange3, orange3,
+            orange2, orange2, orange2, orange2, orange2, orange2, orange2, orange2, orange2, orange2,
+            orange1, orange1, orange1, orange1, orange1, orange1, orange1, orange1, orange1, orange1,
+            red2, red2, red2, red2, red2, red2, red2, red2, red2, red2,
+            red1, red1, red1, red1, red1, red1, red1, red1, red1, red1
+    };
+
+    public static Color[] frame6_60per = {
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            yellow1, yellow1, yellow1, yellow1, yellow1, yellow1, yellow1, yellow1, yellow1, yellow1,
+            orange3, orange3, orange3, orange3, orange3, orange3, orange3, orange3, orange3, orange3,
+            orange2, orange2, orange2, orange2, orange2, orange2, orange2, orange2, orange2, orange2,
+            orange1, orange1, orange1, orange1, orange1, orange1, orange1, orange1, orange1, orange1,
+            red2, red2, red2, red2, red2, red2, red2, red2, red2, red2,
+            red1, red1, red1, red1, red1, red1, red1, red1, red1, red1
+    };
+
+    public static Color[] frame7_70per = {
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            green1, green1, green1, green1, green1, green1, green1, green1, green1, green1,
+            yellow1, yellow1, yellow1, yellow1, yellow1, yellow1, yellow1, yellow1, yellow1, yellow1,
+            orange3, orange3, orange3, orange3, orange3, orange3, orange3, orange3, orange3, orange3,
+            orange2, orange2, orange2, orange2, orange2, orange2, orange2, orange2, orange2, orange2,
+            orange1, orange1, orange1, orange1, orange1, orange1, orange1, orange1, orange1, orange1,
+            red2, red2, red2, red2, red2, red2, red2, red2, red2, red2,
+            red1, red1, red1, red1, red1, red1, red1, red1, red1, red1
+    };
+
+    public static Color[] frame8_80per = {
+            black, black, black, black, black, black, black, black, black, black,
+            black, black, black, black, black, black, black, black, black, black,
+            green2, green2, green2, green2, green2, green2, green2, green2, green2, green2,
+            green1, green1, green1, green1, green1, green1, green1, green1, green1, green1,
+            yellow1, yellow1, yellow1, yellow1, yellow1, yellow1, yellow1, yellow1, yellow1, yellow1,
+            orange3, orange3, orange3, orange3, orange3, orange3, orange3, orange3, orange3, orange3,
+            orange2, orange2, orange2, orange2, orange2, orange2, orange2, orange2, orange2, orange2,
+            orange1, orange1, orange1, orange1, orange1, orange1, orange1, orange1, orange1, orange1,
+            red2, red2, red2, red2, red2, red2, red2, red2, red2, red2,
+            red1, red1, red1, red1, red1, red1, red1, red1, red1, red1
+    };
+
+    public static Color[] frame9_90per = {
+            black, black, black, black, black, black, black, black, black, black,
+            green3, green3, green3, green3, green3, green3, green3, green3, green3, green3,
+            green2, green2, green2, green2, green2, green2, green2, green2, green2, green2,
+            green1, green1, green1, green1, green1, green1, green1, green1, green1, green1,
+            yellow1, yellow1, yellow1, yellow1, yellow1, yellow1, yellow1, yellow1, yellow1, yellow1,
+            orange3, orange3, orange3, orange3, orange3, orange3, orange3, orange3, orange3, orange3,
+            orange2, orange2, orange2, orange2, orange2, orange2, orange2, orange2, orange2, orange2,
+            orange1, orange1, orange1, orange1, orange1, orange1, orange1, orange1, orange1, orange1,
+            red2, red2, red2, red2, red2, red2, red2, red2, red2, red2,
+            red1, red1, red1, red1, red1, red1, red1, red1, red1, red1
+    };
+
+    public static Color[] frame10_100per = {
+            green4, green4, green4, green4, green4, green4, green4, green4, green4, green4,
+            green3, green3, green3, green3, green3, green3, green3, green3, green3, green3,
+            green2, green2, green2, green2, green2, green2, green2, green2, green2, green2,
+            green1, green1, green1, green1, green1, green1, green1, green1, green1, green1,
+            yellow1, yellow1, yellow1, yellow1, yellow1, yellow1, yellow1, yellow1, yellow1, yellow1,
+            orange3, orange3, orange3, orange3, orange3, orange3, orange3, orange3, orange3, orange3,
+            orange2, orange2, orange2, orange2, orange2, orange2, orange2, orange2, orange2, orange2,
+            orange1, orange1, orange1, orange1, orange1, orange1, orange1, orange1, orange1, orange1,
+            red2, red2, red2, red2, red2, red2, red2, red2, red2, red2,
+            red1, red1, red1, red1, red1, red1, red1, red1, red1, red1
+    };
+}
