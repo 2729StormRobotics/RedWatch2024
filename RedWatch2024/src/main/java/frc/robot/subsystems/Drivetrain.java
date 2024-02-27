ca1e2ce31100f3ad03699d98629653d7415811b4
KrithikAlluri <118485611+KrithikAlluri@users.noreply.github.com>
2024-02-26 20:08:43 -0500

    Changed and cleaned up code

--- RedWatch2024/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ RedWatch2024/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -60,7 +60,7 @@ public class Drivetrain extends SubsystemBase {
   private SlewRateLimiter m_rotLimiter = new SlewRateLimiter(DriveConstants.kRotationalSlewRate);
   private double m_prevTime = WPIUtilJNI.now() * 1e-6;
 
-
+  
   // Odometry class for tracking robot pose
   SwerveDriveOdometry m_odometry;
 
