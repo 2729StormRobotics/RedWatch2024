9cd6e0a91c8916ff350fa0b9064fda5c35513cec
KrithikAlluri <118485611+KrithikAlluri@users.noreply.github.com>
2024-09-24 21:45:48 -0400

    1st build

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "RedWatch2024";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 255;
+  public static final String GIT_SHA = "9745e12bc0520ef70c24ec8c8ae9af1f3467285c";
+  public static final String GIT_DATE = "2024-09-24 16:01:59 EDT";
+  public static final String GIT_BRANCH = "AdvantageKitTesting";
+  public static final String BUILD_DATE = "2024-09-24 21:45:33 EDT";
+  public static final long BUILD_UNIX_TIME = 1727228733614L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants() {}
+}
