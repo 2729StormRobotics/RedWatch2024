f73fefd1dfb7e124b662ca9c63ae7c519eae09bf
KrithikAlluri <118485611+KrithikAlluri@users.noreply.github.com>
2024-09-25 08:19:46 -0400

    Change Joystick Type

--- build/spotless/spotlessJava/src/main/java/frc/robot/BuildConstants.java
+++ build/spotless/spotlessJava/src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "RedWatch2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 257;
-  public static final String GIT_SHA = "b7cca6d2d3e79be938538b71c46301a4094fc226";
-  public static final String GIT_DATE = "2024-09-24 22:37:56 EDT";
+  public static final int GIT_REVISION = 258;
+  public static final String GIT_SHA = "b951b83624066b1c4bb6a4f12c76c3f4454e3187";
+  public static final String GIT_DATE = "2024-09-25 08:06:08 EDT";
   public static final String GIT_BRANCH = "AdvantageKitTesting";
-  public static final String BUILD_DATE = "2024-09-25 08:02:59 EDT";
-  public static final long BUILD_UNIX_TIME = 1727265779699L;
+  public static final String BUILD_DATE = "2024-09-25 08:19:38 EDT";
+  public static final long BUILD_UNIX_TIME = 1727266778193L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
