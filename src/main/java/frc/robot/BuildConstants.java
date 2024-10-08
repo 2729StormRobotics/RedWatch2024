0fcbd2c60effa1f1c36f7213c8a718443f3a11ce
AkshayM1 <94143580+RVaman@users.noreply.github.com>
2024-10-08 16:16:05 -0400

    upadte thing

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "RedWatch2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 275;
-  public static final String GIT_SHA = "1ec263e0831c586c36b74dae72e5d5c2d51262ca";
-  public static final String GIT_DATE = "2024-10-01 16:47:48 EDT";
+  public static final int GIT_REVISION = 276;
+  public static final String GIT_SHA = "25e68f52f51bb820af8f58b3a2d7ba17c64ecf37";
+  public static final String GIT_DATE = "2024-10-01 17:05:03 EDT";
   public static final String GIT_BRANCH = "AdvantageKitTesting";
-  public static final String BUILD_DATE = "2024-10-01 17:04:03 EDT";
-  public static final long BUILD_UNIX_TIME = 1727816643318L;
+  public static final String BUILD_DATE = "2024-10-08 16:06:22 EDT";
+  public static final long BUILD_UNIX_TIME = 1728417982392L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
