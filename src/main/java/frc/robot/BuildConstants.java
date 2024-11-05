c7358fbb527a8b89b9c9839ebe1acc7d57f5fb48
PraveenPrabaharan <83364745+PraveenPrabaharan@users.noreply.github.com>
2024-11-05 16:57:21 -0500

    turn pid values hopefully fixed, now tweaking ks and kv

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "RedWatch2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 296;
-  public static final String GIT_SHA = "539797a8a13fce87937ece0fff193e3d09fc54a5";
-  public static final String GIT_DATE = "2024-10-13 19:46:20 EDT";
+  public static final int GIT_REVISION = 297;
+  public static final String GIT_SHA = "f3ea4d268a648f5b637dad354921fcf79b75f387";
+  public static final String GIT_DATE = "2024-10-29 16:50:43 EDT";
   public static final String GIT_BRANCH = "AdvantageKitTesting";
-  public static final String BUILD_DATE = "2024-10-29 16:47:56 EDT";
-  public static final long BUILD_UNIX_TIME = 1730234876253L;
+  public static final String BUILD_DATE = "2024-11-05 16:48:30 EST";
+  public static final long BUILD_UNIX_TIME = 1730843310966L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/subsystems/drive/Module.java
+++ src/main/java/frc/robot/subsystems/drive/Module.java
@@ -46,6 +46,10 @@ public class Module {
     // separate robot with different tuning)
     switch (Constants.currentMode) {
       case REAL:
+        driveFeedforward = new SimpleMotorFeedforward(0.1, 0.04);
+        driveFeedback = new PIDController(0.005, 0.0, 0.0);
+        turnFeedback = new PIDController(2.6, 0.0, 0.025);
+        break;
       case REPLAY:
         driveFeedforward = new SimpleMotorFeedforward(0.1, 0.13);
         driveFeedback = new PIDController(0.05, 0.0, 0.0);
